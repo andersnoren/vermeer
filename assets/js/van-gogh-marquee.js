35e0473a85c84760aeefc3fe655a762395ecd535
Anders <anders@andersnoren.se>
2024-04-11 21:14:26 +0200

    Progress on marquee

--- assets/js/van-gogh-marquee.js
+++ assets/js/van-gogh-marquee.js
@@ -1,23 +1,38 @@
 function Marquee( selector, speed ) {
-	const parentSelector = document.querySelector(selector);
+
+	const parentSelector = document.querySelector( selector );
 	const parentHTML     = parentSelector.innerHTML;
+	const screenWidth    = window.innerWidth;
 
-	// Build html
-	parentSelector.innerHTML = '<div>' + parentHTML + '&nbsp;</div>' + '<div>' + parentHTML + '&nbsp;</div>' + '<div>' + parentHTML + '&nbsp;</div>';
+	// Add an inner div that will wrap the clones and will be animated across the screen.
+	parentSelector.innerHTML = '<div class="slider">' + parentHTML + ' </div>';
+	var parentSlider = parentSelector.children[0];
 
-	const children = parentSelector.children;
-	
+	// Build the HTML.
+	let newInnerHTML = '<div>' + parentHTML + '&nbsp;</div>';
+	parentSlider.innerHTML = newInnerHTML;
+
+	// Clone the content until it's wide enough to cover the screen width.
+	let sliderWidth = 0;
+	while ( sliderWidth < screenWidth ) {
+		sliderWidth = parentSlider.getBoundingClientRect().width;
+		parentSlider.innerHTML += newInnerHTML;
+	}
+
+	// Animate the section.
+	var children = parentSlider.children;
 	let i = 0;
 
 	setInterval( function() {
 		for ( var child = 0; child < children.length; child++ ) {
-			children[child].style.transform = `translateX( -${i}px )`;
+			parentSlider.style.transform = `translateX( -${i}px )`;
 		}
-		if ( i > children[0].clientWidth ) {
+		if ( i > children[0].getBoundingClientRect().width ) {
 			i = 0;
 		}
-		i = i + speed;
+		parentSlider.style.transform = `translateX( -${i}px )`;
+		i += speed;
 	}, 0 );
 }
 
-window.addEventListener( 'load', Marquee( '.is-style-van-gogh-marquee', .25 ) );
+window.addEventListener( 'load', Marquee( '.is-style-van-gogh-marquee', .5 ) );
